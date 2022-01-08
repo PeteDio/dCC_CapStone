a58786016ba10b24af834fd40fefd60b45f0a871
PeteDillo <91334249+PeteDillo@users.noreply.github.com>
2022-01-07 18:27:28 -0600

    updated the styling for the home scroll.

--- src/components/Meals/Meals.jsx
+++ src/components/Meals/Meals.jsx
@@ -1,16 +1,19 @@
 import React, { useState, useEffect } from "react";
-import './meals.scss'
+import "./meals.scss";
 import axios from "axios";
 import MealDetails from "../MealDetails/MealDetails";
 const Meals = ({ meal }) => {
-  const {recipe, datetime, caption } = meal;
+  const { recipe, datetime, caption } = meal;
   const [comments, setComments] = useState([]);
 
   const getComments = async () => {
     try {
       console.log("All meals response: ", meal);
       const jwt = localStorage.getItem("token");
-      let res = await axios.get(`http://127.0.0.1:8000/api/comments/mealid/16/`, { headers: { Authorization: "Bearer " + jwt }});
+      let res = await axios.get(
+        `http://127.0.0.1:8000/api/comments/mealid/16/`,
+        { headers: { Authorization: "Bearer " + jwt } }
+      );
       console.log("All comments response: ", res.data);
       setComments(res.data);
       // console.log(meals)
@@ -24,19 +27,17 @@ const Meals = ({ meal }) => {
     getComments();
   }, []);
 
-
-  
-
   return (
     <div className="meal">
-      <span>{caption}</span>
-      <button>{recipe}</button>
+      <button className="recipeLink">{recipe}</button>
+      <span className="caption">{caption}</span>
       <span>{datetime}</span>
-      {comments.map(comment => <MealDetails comment={comment} />)}
-      {console.log('comments from the return in meals.jsx ' + comments[0])}
-      
-      
-
+      <div className="comments">
+        {comments.map((comment) => (
+          <MealDetails comment={comment} />
+        ))}
+        {console.log("comments from the return in meals.jsx " + comments[0])}
+      </div>
     </div>
   );
 };
